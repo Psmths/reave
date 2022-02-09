89263fc3a190d72bb3137dcb7bee9426274df7b4
Psmths <psmth@protonmail.ch>
2022-02-09 12:18:42 -0600

    update installer

--- install.sh
+++ install.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
 function generate_certificate() {
+    echo "Generating certificate using config at" $(dirname "$0")/reave/conf/csr.conf
     mkdir -p $(dirname "$0")/reave/data
-    openssl req -new -x509 -days 365 -nodes -out $(dirname "$0")/reave/data/cert.pem -keyout $(dirname "$0")/reave/data/cert.pem
+    openssl req -config $(dirname "$0")/reave/conf/csr.conf \
+    -new -x509 -days 365 -nodes -out $(dirname "$0")/reave/data/cert.pem \
+    -keyout $(dirname "$0")/reave/data/cert.pem \
+    >/dev/null 2>&1
+}
+
+function chkconfig(){
+    CONFIG_FILE=$(dirname "$0")/reave/conf/reave.conf
+    if [ -f "$CONFIG_FILE" ]; then
+        echo "$CONFIG_FILE already loaded."
+    else 
+        echo "$CONFIG_FILE does not exist, creating it"
+        cp $(dirname "$0")/reave/resource/reave.conf.default $CONFIG_FILE
+    fi
 }
 
 generate_certificate
+chkconfig
